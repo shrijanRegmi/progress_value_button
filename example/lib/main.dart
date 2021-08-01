e032b73621d9ea7a1ae62c82e2ed64a5cdb40a47
Shrijan Regmi <ilyyhs9@gmail.com>
2021-08-01 10:01:52 +0545

    minor changes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -20,27 +20,45 @@ class ProgressButtonApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _loaded = false;
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(milliseconds: 2000),
+      () => setState(() => _loaded = true),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ProgressButton(
-          value: 90.0,
-          height: 65.0,
-          onPressed: (progress) {},
-          margin: const EdgeInsets.all(10.0),
-          child: Text(
-            "Uploading...",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              fontSize: 18.0,
-            ),
-          ),
-        ),
+        child: _loaded
+            ? ProgressButton(
+                value: 100.0,
+                animationDuration: Duration(milliseconds: 8000),
+                height: 65.0,
+                onPressed: (progress) {},
+                margin: const EdgeInsets.all(10.0),
+                child: Text(
+                  "Uploading...",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              )
+            : Container(),
       ),
     );
   }
